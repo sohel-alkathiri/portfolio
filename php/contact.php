83485e1bdf4a4ca68d27bff5a7fc9e8bd0548c7f
Sohel Alkathiri <sohel.alkathiri@gmail.com>
2025-11-05 14:26:52 +0530

    PHPMailer added to contact form

--- php/contact.php
+++ php/contact.php
@@ -1,9 +1,18 @@
 <?php
+// PHP Mailer Function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require 'Exception.php'; 
+require 'PHPMailer.php';
+require 'SMTP.php';
+
 // content type header to JSON
 header('Content-Type: application/json');
 
 $to_email = 'dev.sohelchaus@gmail.com';
-$website_name = "My Portfolio Website"; 
+$website_name = "My Portfolio"; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
 
@@ -21,7 +30,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
     }
 
     // Email Validation (Form Validation)
-    $email_subject = "New Inquiry from " . $website_name . " (" . ucfirst($subject) . ")";
+    $email_subject = "Inquiry from " . $website_name . " (" . ucfirst($subject) . ")";
     
     $email_body    = "You have received a new message from your contact form.\n\n";
     $email_body   .= "Name: " . $name . "\n";
@@ -29,6 +38,40 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST)) {
     $email_body   .= "Phone: " . (empty($phone) ? 'N/A (Optional)' : $phone) . "\n";
     $email_body   .= "Subject: " . ucfirst($subject) . "\n\n";
     $email_body   .= "--- Message ---\n" . $message . "\n-----------------\n";
+
+    // PHPMailer - Error Handling
+$mail = new PHPMailer(true); // Create a new PHPMailer object
+
+try {
+    // Server settings
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.gmail.com'; 
+    $mail->SMTPAuth   = true;
+    $mail->Username   = 'dev.sohelchaus@gmail.com';
+    $mail->Password   = 'mqkk nyim iibf zkjg';
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+    $mail->Port       = 465;                        
+
+    // Recipients
+    $mail->setFrom($email, $name);     
+    $mail->addAddress($to_email);      // Sends to your defined $to_email
+
+    // Content
+    $mail->isHTML(false); // Plain text
+    $mail->Subject = $email_subject;
+    $mail->Body    = $email_body;
+
+    $mail->send();
+    
+    // Success response for JavaScript
+    http_response_code(200);
+    echo json_encode(['success' => true, 'message' => 'Message Sent Successfully!']);
+
+    } catch (Exception $e) {
+    // Failure response for JavaScript (catches errors during connection/authentication)
+    http_response_code(500);
+    echo json_encode(['success' => false, 'message' => "Server error: Message could not be sent. Mailer Error: {$mail->ErrorInfo}"]);
+}
 
     // 5. Define Email Header
     $headers  = 'From: ' . $website_name . ' <noreply@yourdomain.com>' . "\r\n";
